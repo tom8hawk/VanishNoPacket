97e762f14c28217db8e9893aa3ace7c702095b94
Brokkonaut <hannos17@gmx.de>
2022-06-22 04:33:10 +0200

    Cancel stepping on sculk sensors/shriekers

--- src/main/java/org/kitteh/vanish/listeners/Listen119.java
+++ src/main/java/org/kitteh/vanish/listeners/Listen119.java
@@ -5,7 +5,10 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockReceiveGameEvent;
+import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.kitteh.vanish.VanishPlugin;
 
 public final class Listen119 implements Listener {
@@ -20,6 +23,16 @@ public final class Listen119 implements Listener {
         Entity trigger = event.getEntity();
         if (trigger instanceof Player && event.getBlock().getType() == Material.SCULK_SENSOR) {
             if (this.plugin.getManager().isVanished((Player) trigger)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Material blockType;
+        if (event.getAction() == Action.PHYSICAL && ((blockType = event.getClickedBlock().getType()) == Material.SCULK_SENSOR || blockType == Material.SCULK_SHRIEKER)) {
+            if (this.plugin.getManager().isVanished(event.getPlayer())) {
                 event.setCancelled(true);
             }
         }
